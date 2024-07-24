f4e89a0fdf0562f5933e606f2fd4d83838e7493b
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-07-24 21:59:39 +0530

    compiling. next step is to add solr cloud resource from the generated crd and also fix the ingress pieces and exports

--- Makefile
+++ Makefile
@@ -23,3 +23,4 @@ update-deps:
 	go get github.com/plantoncloud/planton-cloud-apis@latest
 	go get github.com/plantoncloud/stack-job-runner-golang-sdk
 	go get github.com/plantoncloud/pulumi-module-golang-commons
+	go get github.com/plantoncloud/kubernetes-crd-pulumi-types
--- pkg/main.go
+++ pkg/main.go
@@ -5,6 +5,8 @@ import (
 	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/kubernetes/solrkubernetes/model"
 	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/commons/kubernetes/enums/kubernetesworkloadingresstype"
 	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/provider/kubernetes/pulumikubernetesprovider"
+	kubernetescorev1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/core/v1"
+	kubernetesmetav1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
@@ -13,27 +15,31 @@ type ResourceStack struct {
 	Labels map[string]string
 }
 
-func (resourceStack *ResourceStack) Resources(ctx *pulumi.Context) error {
+func (s *ResourceStack) Resources(ctx *pulumi.Context) error {
 	//create kubernetes-provider from the credential in the stack-input
 	kubernetesProvider, err := pulumikubernetesprovider.GetWithKubernetesClusterCredential(ctx,
 		s.Input.KubernetesClusterCredential)
 	if err != nil {
-		return errors.Wrap(err, "failed to setup gcp provider")
+		return errors.Wrap(err, "failed to create kubernetes provider")
 	}
 
 	//create a new descriptive variable for the api-resource in the input.
-	jenkinsKubernetes := s.Input.ApiResource
+	solrKubernetes := s.Input.ApiResource
 
 	//decide on the name of the namespace
-	namespaceName := jenkinsKubernetes.Metadata.Id
+	namespaceName := solrKubernetes.Metadata.Id
 
 	//create namespace resource
-	createdNamespace, err := kubernetescorev1.NewNamespace(ctx, namespaceName, &kubernetescorev1.NamespaceArgs{
-		Metadata: metav1.ObjectMetaPtrInput(&metav1.ObjectMetaArgs{
-			Name:   pulumi.String(namespaceName),
-			Labels: pulumi.ToStringMap(s.KubernetesLabels),
-		}),
-	}, pulumi.Timeouts(&pulumi.CustomTimeouts{Create: "5s", Update: "5s", Delete: "5s"}),
+	createdNamespace, err := kubernetescorev1.NewNamespace(ctx,
+		namespaceName,
+		&kubernetescorev1.NamespaceArgs{
+			Metadata: kubernetesmetav1.ObjectMetaPtrInput(
+				&kubernetesmetav1.ObjectMetaArgs{
+					Name:   pulumi.String(namespaceName),
+					Labels: pulumi.ToStringMap(s.Labels),
+				}),
+		},
+		pulumi.Timeouts(&pulumi.CustomTimeouts{Create: "5s", Update: "5s", Delete: "5s"}),
 		pulumi.Provider(kubernetesProvider))
 	if err != nil {
 		return errors.Wrapf(err, "failed to create %s namespace", namespaceName)
@@ -42,30 +48,24 @@ func (resourceStack *ResourceStack) Resources(ctx *pulumi.Context) error {
 	//export name of the namespace
 	ctx.Export(NamespaceOutputName, createdNamespace.Metadata.Name())
 
-	//create admin-password secret
-	createdAdminPasswordSecret, err := s.adminPassword(ctx, createdNamespace)
-	if err != nil {
-		return errors.Wrap(err, "failed to create admin password resources")
-	}
-
-	//install the jenkins helm-chart
-	if err := s.helmChart(ctx, createdNamespace, createdAdminPasswordSecret); err != nil {
+	//install the solr helm-chart
+	if err := s.solrCloud(ctx, createdNamespace); err != nil {
 		return errors.Wrap(err, "failed to create helm-chart resources")
 	}
 
 	//export kube-port-forward command
 	ctx.Export(PortForwardCommandOutputName, pulumi.Sprintf(
 		"kubectl port-forward -n %s service/%s 8080:8080",
-		namespaceName, jenkinsKubernetes.Metadata.Name))
+		namespaceName, solrKubernetes.Metadata.Name))
 
 	//no ingress resources required when ingress is not enabled
-	if !jenkinsKubernetes.Spec.Ingress.IsEnabled || jenkinsKubernetes.Spec.Ingress.EndpointDomainName == "" {
+	if !solrKubernetes.Spec.Ingress.IsEnabled || solrKubernetes.Spec.Ingress.EndpointDomainName == "" {
 		return nil
 	}
 
 	//depending on the ingress-type in the input, create either istio-ingress resources or
 	//create load-balancer resources
-	switch jenkinsKubernetes.Spec.Ingress.IngressType {
+	switch solrKubernetes.Spec.Ingress.IngressType {
 	case kubernetesworkloadingresstype.KubernetesWorkloadIngressType_load_balancer:
 		if err := s.loadBalancerIngress(ctx, createdNamespace); err != nil {
 			return errors.Wrap(err, "failed to create load-balancer ingress resources")
@@ -78,10 +78,9 @@ func (resourceStack *ResourceStack) Resources(ctx *pulumi.Context) error {
 
 	//export ingress hostnames
 	ctx.Export(IngressExternalHostnameOutputName, pulumi.Sprintf("%s.%s",
-		jenkinsKubernetes.Metadata.Id, jenkinsKubernetes.Spec.Ingress.EndpointDomainName))
+		solrKubernetes.Metadata.Id, solrKubernetes.Spec.Ingress.EndpointDomainName))
 	ctx.Export(IngressInternalHostnameOutputName, pulumi.Sprintf("%s-internal.%s",
-		jenkinsKubernetes.Metadata.Id, jenkinsKubernetes.Spec.Ingress.EndpointDomainName))
+		solrKubernetes.Metadata.Id, solrKubernetes.Spec.Ingress.EndpointDomainName))
 
-	return nil
 	return nil
 }
